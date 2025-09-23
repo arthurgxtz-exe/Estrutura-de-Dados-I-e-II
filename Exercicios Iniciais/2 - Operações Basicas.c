270d5b68a9b08f1a03e45e0bc4b99d1d72dad58f
Arthur <arthurferreira431@gmail.com>
2025-09-23 11:22:03 -0300

    Resolvendo o Exercicio 2

---
+++ Exercicios Iniciais/2 - Operações Basicas.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+
+    int num1, num2;
+    int soma, sub, mult, qs;
+    float div;
+
+    printf("Digite o primeiro número:");
+    scanf("%d", &num1);
+    printf("Digite o segundo número:");
+    scanf("%d", &num2);
+
+    soma = num1 + num2;
+    sub = num1 - num2;
+    mult = num1 * num2;
+    div = (float)num1 / num2;
+    qs = (num1 - num2) * (num1 - num2);
+
+    printf("\n--- Resultados ---\n");
+    printf("Soma: %d\n", soma);
+    printf("Subtracao: %d\n", sub);
+    printf("Multiplicacao: %d\n", mult);
+    printf("Divisao: %.2f\n", div);
+    printf("Quadrado da subtracao: %d\n", qs);
+}
