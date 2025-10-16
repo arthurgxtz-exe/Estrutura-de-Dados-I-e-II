e1f3208b2e0deb4f73aed708143981a9c91e2f69
Arthur <arthurferreira431@gmail.com>
2025-10-16 20:57:41 -0300

    Arrays - Preenche e imprime matriz NxN

---
+++ Arrays/Exercicio 5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n, x;
+
+    printf("Digite um valor, para ser o  tamanho da tabela:\n");
+    scanf("%d", &n);
+    fflush(stdin);
+    printf("A tabela será prenchida com n° aleatórios entre 1 e ...?\n");
+    scanf("%d", &x);
+    fflush(stdin);
+    system("cls");
+
+    int matriz[n][n];
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            matriz[i][j] = rand() % x + 1;
+        }
+    }
+            
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {   
+            printf("%d\t", matriz[i][j]);
+        }
+
+       printf("\n");
+    }
+return 0;
+}
