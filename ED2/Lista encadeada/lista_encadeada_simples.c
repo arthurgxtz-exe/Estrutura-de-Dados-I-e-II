d6fec6d60000457d95c9ea2f77e0d1dd23bfb358
Arthur Almeida <almeida.arthur.ti@gmail.com>
2026-05-07 14:24:20 -0300

    lista encadeada simples

--- ED2/Lista encadeada/lista_encadeada_simples.c
+++ ED2/Lista encadeada/lista_encadeada_simples.c
@@ -67,10 +67,47 @@ void inserir_meio(No **lista, int num, int ant){
      
 }
 
+void imprimir_lista(No *head){
+    printf("\nLista de valores\n");
+
+    No *atual = head;
+
+    while(atual != NULL){
+        printf("%d \n", atual->valor);
+        atual = atual->proximo;
+    }
+}
+
+void menu(){
+    int op;
+    printf("1 - Inserir no inicio\n");
+    printf("2 - Inserir no meio \n");
+    printf("3 - Inserir no Final\n");
+    printf("4 - Listar valores da lista\n");
+    scanf("%d", &op);
+    return op;
+}
+
 int main() {
 
     No lista;
+    int op;
+    do{
+        op = menu();
+        switch(op){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            default:
+                break;
+        }
 
+    }while(op != 0);
     
     return 0;
 }
