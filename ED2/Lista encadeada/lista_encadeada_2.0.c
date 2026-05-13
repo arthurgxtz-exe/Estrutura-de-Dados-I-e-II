014ca5efba447d4e9c96c883c18a394715839bb3
Arthur Almeida <almeida.arthur.ti@gmail.com>
2026-05-13 10:24:23 -0300

    implementando função de dar prioridade

--- ED2/Lista encadeada/lista_encadeada_2.0.c
+++ ED2/Lista encadeada/lista_encadeada_2.0.c
@@ -112,9 +112,97 @@ int menu(){
     printf("2 - Inserir no meio \n");
     printf("3 - Inserir no Final\n");
     printf("4 - Listar valores da lista\n");
+    printf("5 - Remover um valor da lista\n");
+    printf("6 - Dar prioridade á um valor\n");;
     printf("0 - SAIR\n");
     scanf("%d", &op);
     return op;
+}
+
+void remover_no(Lista *l, int alvo){
+    if(l->head == NULL){
+        printf("Não existe nenhum valor na lista\n");
+        return;
+    }
+    No *aux = l->head;
+    No *anterior;
+
+    while (aux != NULL && aux->valor != alvo){
+
+        anterior = aux;
+        aux = aux->proximo;
+        
+    }
+    if(aux == NULL){
+        printf("Valor não encontrado.\n");
+
+    }
+
+    if(anterior == NULL){
+        l->head = aux->proximo;        
+    }
+    else{
+        anterior->proximo = aux->proximo;
+    }
+    if(aux->proximo == NULL){
+        l->tail = anterior;
+    }
+
+    free(aux);
+    l->tam--;
+    printf("Valor removido com sucesso\n");
+
+}
+
+void dar_prioridade(Lista *l, int alvo){
+
+    if(l->head == NULL){
+        printf("Não existe nenhum valor na lista\n");
+        return;
+    }
+
+    No *aux = l->head;
+    No *anterior;
+
+    while(aux != NULL && aux->valor != alvo){
+        anterior = aux;
+        aux = aux->proximo;
+    }
+
+    if(anterior == NULL){
+        printf("O valor já está no topo da lista\n");
+        return;
+    }
+    else{
+        anterior->proximo = aux->proximo;
+    }
+    if(aux->proximo == NULL){
+        l->tail = anterior;
+
+    }
+
+    aux->proximo = l->head;
+    l->head = aux;
+    
+    printf("O valor foi movido para o inicio com sucesso!");
+
+}
+
+void finalizar_lista(Lista *l){
+    No *aux = l->head;
+    No *temp;
+
+    while( aux != NULL){
+        temp = aux->proximo;
+        free(aux);
+        aux = temp;
+    }
+
+    l->head = NULL;
+    l->tail = NULL;
+    l->tam = 0;
+
+    printf("Memoria limpa com sucesso.\n");
 }
 
 int main() {
@@ -146,8 +234,19 @@ int main() {
             case 4:
                 imprimir_lista(&lista);
                 break;
+            case 5:
+                printf("Digite o valor que deseja remover:\n");
+                scanf("%d", &valor);
+                remover_no(&lista, valor);
+                break;
+            case 6:
+                printf("Digite o valor que você deseja dá prioridade: ");
+                scanf("%d", &valor);
+                dar_prioridade(&lista, valor);
+                break;
             case 0: 
-                printf("Encerrando programa...\n");
+                printf("Limpando memoria e Encerrando programa...\n");
+                finalizar_lista(&lista);
                 break;
             default:
                 printf("Opção invalida!");
